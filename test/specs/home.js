3975886346c49212125d746b44f72b5f6e7dcbf8
Vukasin Grebic <vgrebic@gmail.com>
2023-10-17 18:39:00 +0200

    Added Cart Tests

--- test/specs/home.js
+++ test/specs/home.js
@@ -1,10 +1,8 @@
 import HomePage from "../pages/home-page.js"
 
 describe('Homepage flow', () => {
-    beforeEach(async () => {
-        await HomePage.open()
-    })
     
+
     it('Visiting home page and checking url', async () => {
         await HomePage.open()
         await expect(browser).toHaveUrlContaining("amazon")
